58184532fe2ae8850154f3e54843c01eae686072
Daniel van der Garde <daan@cart2quote.com>
2020-07-01 11:58:29 +0200

    Fixed: Diff from two remotes doesn't work Fixed: diff.renameLimit isn't set in working directory Added: Generation of migrate script Added: migrate script can execute the generated patch now

--- create-patches.sh
+++ create-patches.sh
@@ -7,22 +7,30 @@ if ! [[ -d patches ]]; then
     git clone -n https://github.com/OpenMage/magento-lts.git patches; \
     cd patches; \
     git remote add mirror https://github.com/OpenMage/magento-mirror.git; \
+    git remote update; \
   )
 fi
 
-git fetch --all
-git config diff.renameLimit 10000
-
 cd patches
-read -p "Create patches for which OpenMage LTS version? (e.g. v19.4.3) " target
+git fetch --all
+git config diff.renameLimit 100000
+
+read -p "Create patches for which OpenMage LTS version? (e.g. v19.4.4) " target
 echo "Will create patches for all versions to $target"
 git log -1 $target | head -n 3
 read -p "Confirm the commit above is correct and press enter to continue." input
+targethash=$(git rev-parse $target)
 
 for tag in $(git ls-remote --tags mirror | awk '{print $2}' | grep -vE "alpha|beta|rc|{}"); do
-  echo "Creating patch for $tag..."
-  git diff --binary $tag..$openmage > magento-ce-${tag##*/}-openmage-lts-$target.patch
+  taghash=$(git ls-remote --tags mirror | grep -vE "alpha|beta|rc" | grep $tag | tail -1 | awk '{print $1}')
+  echo "Creating patch for $tag..$target ($taghash..$targethash)..."
+  git diff --binary $taghash..$targethash > magento-ce-${tag##*/}-openmage-lts-$target.patch
 done
 
 # Sync to S3
 # TODO
+
+# Generate the new migrate script
+cd ../
+sed "s/SCRIPT_COMMIT_SHA_HERE/$(git log -1 --format=%H)/" migrate.sh > upgrade.sh
+echo "Execute 'bash upgrade.sh' to upgrade to OpenMage $target."
